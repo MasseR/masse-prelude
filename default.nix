ab431de3e5b091d9277d745e6e22e051573e8cae
Mats Rauhala <mats.rauhala@iki.fi>
2019-12-18 21:08:53 +0200

    Initial prelude

--- default.nix
+++ default.nix
@@ -1,8 +1,12 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, base, bytestring, containers, mtl, stdenv, text
+, time, unliftio
+}:
 mkDerivation {
   pname = "masse-prelude";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base ];
+  libraryHaskellDepends = [
+    base bytestring containers mtl text time unliftio
+  ];
   license = stdenv.lib.licenses.mit;
 }
--- src/MyPrelude.hs
+++ src/MyPrelude.hs
@@ -1,4 +1,49 @@
-module MyPrelude () where
+module MyPrelude
+  ( module X
+  , tshow
+  , unlessM
+  , ByteString
+  , LByteString
+  , note
+  )
+  where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Control.Monad        as X (filterM, forM, forM_, forever, join,
+                                            unless, void, (<=<))
+import           Control.Monad.Reader as X (MonadReader, ReaderT (..), ask,
+                                            asks, runReaderT)
+import           Data.Bool            as X (bool)
+import           Data.Foldable        as X (foldl', for_, traverse_)
+import           Data.Int             as X (Int64)
+import           Data.List            as X (intercalate, isInfixOf)
+import           Data.Map.Strict      as X (Map)
+import           Data.Maybe           as X (isJust, isNothing)
+import           Data.Set             as X (Set, member)
+import           Data.Text            as X (Text, pack, unpack)
+import           Data.Text.Encoding   as X (encodeUtf8)
+import           Data.Text.IO         as X (putStrLn)
+import           Data.Time            as X (Day, UTCTime (..),
+                                            defaultTimeLocale, formatTime,
+                                            getCurrentTime)
+import           GHC.Generics         as X (Generic)
+import           Prelude              as X hiding (lookup, putStrLn, readFile)
+import           UnliftIO             as X hiding (Handler)
+
+import           Data.ByteString      as X (readFile)
+import qualified Data.ByteString      as B
+import qualified Data.ByteString.Lazy as BL
+
+type ByteString = B.ByteString
+type LByteString = BL.ByteString
+
+-- | Like 'show' but with 'Text'
+tshow :: Show a => a -> Text
+tshow = pack . show
+
+-- | A monadic 'unless'
+unlessM :: Monad m => m Bool -> m () -> m ()
+unlessM predicate act = predicate >>= \p -> unless p act
+
+-- | Lift a 'Maybe a' into 'Either e a'
+note :: e -> Maybe a -> Either e a
+note e = maybe (Left e) Right
